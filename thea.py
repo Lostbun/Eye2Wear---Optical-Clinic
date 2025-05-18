890180b34d96cc8f60c253e987cff118032d4383
Francis Daniel Genese <tristandivider8@gmail.com>
2025-05-18 22:23:30 +0800

    Modified thea.py added api fetching

--- thea.py
+++ thea.py
@@ -1,7 +1,28 @@
-import numpy as np
+# pip install requests
+import requests
 
-a = np.arange(10, 1, -2) 
-print("\n A sequential array with a negative step: \n",a)
+# Send a GET request to a website
+res = requests.get(
+    "https://www.example.com/", # The URL of the API you want to access
+    params={"key1": "value1", "key2": "value2"}, # The parameters you want to pass to the API (like "?key=value" at the end of the URL)
+    data={"key1": "value1", "key2": "value2"}, # The data you want to send to the API
+    headers={"header1": "value1", "header2": "value2"}, # The headers you want to send to the API
+    cookies={"cookie1": "value1", "cookie2": "value2"}, # The cookies you want to send to the API
+    auth=("username", "password"), # The authentication credentials you want to send to the API (some websites require this)
+    timeout=5, # The maximum site response time (in seconds) 
+    allow_redirects=True, # Whether or not to follow redirects
+)
 
-newarr = a[np.array([3, 1, 2 ])]
-print("\n Elements at these indices are:\n",newarr)
+# Send a POST request to a website
+res = requests.post(...)
+
+# Send a PUT request to a website
+res = requests.put(...)
+
+# Send a DELETE request to a website
+res = requests.delete(...)
+
+
+if res.status_code == 200:
+    output = res.json()
+    print(output)
