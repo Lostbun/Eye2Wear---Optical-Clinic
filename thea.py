05522569c22d643e8708f9f349b974fb7b7e376f
Francis Daniel Genese <tristandivider8@gmail.com>
2025-05-23 17:04:29 +0800

    modified thea.py reemoved the heart code

--- thea.py
+++ thea.py
@@ -9,16 +9,4 @@ def curve():
         pen.right(1)
         pen.forward(1)
 
-# Defining method to draw a full heart
-def heart():
-
-    pen.fillcolor('red')
-    pen.begin_fill()
-    pen.left(140)
-    pen.forward(113)
-    curve()
-    pen.left(120)
-    curve()
-    pen.forward(112)
-    pen.end_fill()
 
