e03e539e3c7278c62a1542de670942ed6cebbab8
Francis Daniel Genese <tristandivider8@gmail.com>
2025-05-29 13:00:00 +0000

    Commit for May 29 (PH Time)

--- thea.py
+++ thea.py
@@ -12,11 +12,12 @@ for step in range(15):
     for dash in range(8):
         penup()
         forward(10)
-        pendown()        forward(10)
+        pendown()        
         pendown()
-        forward(10)        forward(10)
+        forward(10)        
         pendown()
         forward(10)
+        
         forward(10)
     penup()
     backward(160)
