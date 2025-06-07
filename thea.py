60dead45b4e1888844ddf1ce2cbecd6fe80a82c8
Francis Daniel Genese <tristandivider8@gmail.com>
2025-06-07 23:08:18 +0800

    removed if else

--- thea.py
+++ thea.py
@@ -39,8 +39,3 @@ while True:
 
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
