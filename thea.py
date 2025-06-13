4ea031e7c2ae5b7e9cc53e6159a4964625e4ff12
Francis Daniel Genese <tristandivider8@gmail.com>
2025-06-13 13:00:00 +0000

    Commit for June 13 (PH Time)

--- thea.py
+++ thea.py
@@ -30,7 +30,7 @@ while True:
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
-        except ValueError:
+    
 
 
   
